fdf9de81e8c0bec49776bad0e9f1a7d44b39d641
Mark van Dijk <markvandijk@happyhorizon.com>
2024-01-26 08:49:19 +0100

    Created EnhancedEcommerceService v1.6.0

--- src/EnhancedEcommerceService.js
+++ src/EnhancedEcommerceService.js
@@ -1,4 +1,4 @@
-//version = "V 1.6.0 UNRELEASED" Add UNRELEASED if the current version is not yet published to the CDN. When releasing remove UNRELEASED.
+//version = "V 1.6.0" Add UNRELEASED if the current version is not yet published to the CDN. When releasing remove UNRELEASED.
 
 class IndexDataSelector extends DataSelector {
     /**
